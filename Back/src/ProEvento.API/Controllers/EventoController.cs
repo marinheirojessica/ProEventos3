b1929fd4b1bad18e66d2d125cb340bd422d61c2d
marinheirojessica <marinheirojessi20@gmail.com>
2023-12-06 18:46:14 -0300

    Feita algumas alterações. Foi feita todas as referências. Criado o banco de dados.

--- Back/src/ProEvento.API/Controllers/EventoController.cs
+++ Back/src/ProEvento.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEvento.API.Data;
 using ProEvento.API.Models;
 
 namespace ProEvento.API.Controllers
@@ -13,38 +14,23 @@ namespace ProEvento.API.Controllers
     public class EventoController : ControllerBase
     {
         
-        public IEnumerable<Evento> _evento =  new Evento[]{
-                new Evento() {
-                EventoId = 1,
-                Local = "Salvador", 
-                Tema = "Angular 11",
-                Lote = "1° Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(), ImagemURL = "foto1.png"
-            },
-               new Evento() {
-                EventoId = 2,
-                Local = "Petrolina", 
-                Tema = "Angular 17 e suas novidades",
-                Lote = "2° Lote",
-                QtdPessoas = 300,
-                DataEvento = DateTime.Now.AddDays(3).ToString(), ImagemURL = "foto2.png"
-            }
-            };
-        public EventoController()
+       
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
             
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-           return _evento;
+           return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-           return _evento.Where(evento => evento.EventoId == id);
+           return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
           [HttpPost]
         public string Post()
