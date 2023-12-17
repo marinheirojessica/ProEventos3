6aaab4ede5e2e8e4d4a873a6bacaa4762211660c
marinheirojessi <marinheirojessi20@gmail.com>
2023-12-17 00:19:17 -0300

    Criei o front, fiz algumas implementações.

--- Back/src/ProEvento.API/Startup.cs
+++ Back/src/ProEvento.API/Startup.cs
@@ -32,6 +32,7 @@ namespace ProEvento.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEvento.API", Version = "v1" });
@@ -53,6 +54,10 @@ namespace ProEvento.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
